7d420b88c4fa642124745efa222f2b6833842ec2
Arjuna Sharma <as6608936@gmail.com>
2025-11-19 18:21:15 +0545

    Add files via upload

---
+++ file1.php
@@ -0,0 +1,17 @@
+
+<html>
+<head>
+    <title>Upload File</title>
+</head>
+<body>
+
+<h2>Upload an Image File</h2>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <label>Select a file:</label><br><br>
+    <input type="file" name="file" required><br><br>
+    <button type="submit">Upload File</button>
+</form>
+
+</body>
+</html>
